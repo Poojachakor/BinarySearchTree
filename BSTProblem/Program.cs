7508f72e6982a91cd54c12e6ee33922af0b0f0c1
Poojachakor <chakorpooja07@gmail.com>
2023-06-19 16:58:46 +0530

    Add: BSTProblem

---
+++ BSTProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BSTProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Search Tree!");
+            BST binarySearchTree = new BST();
+            binarySearchTree.Insert(56);
+            binarySearchTree.Insert(30);
+            binarySearchTree.Insert(70);
+            binarySearchTree.Display();
+        }
+    }
+}
